10e07740f53fb0a8cbe6e74c68cfe98745e270a8
Damian Edwards <damian@damianedwards.com>
2022-12-02 13:09:07 -0800

    Added second "page"

--- src/MinimalRazorComponents/Program.cs
+++ src/MinimalRazorComponents/Program.cs
@@ -5,5 +5,6 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => Results.Extensions.Component<HelloWorld>(new { Message = "Hello from Minimal APIs" }));
+app.MapGet("/about", () => Results.Extensions.Component<About>());
 
 app.Run();
